07953d65b384a7cc06b75cb32d2dee486a2674eb
Allaye <kolade199@gmail.com>
2021-07-10 23:01:06 +0000

    ..

--- How2Codit/celeryemail/sendemail/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .tasks import send_email
-
-# Create your views here.
-def index(request):
-    send_email.delay()
-    return HttpResponse('<h1>Task 1 of sending email with celery!</h1>')
