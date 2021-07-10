344a84e55c277ccf3f69bb0c097670b7009a1ed8
Allaye <kolade199@gmail.com>
2021-07-10 22:58:11 +0000

    ..

--- fileupload/progress/models.py
+++ fileupload/progress/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=50, unique=True)
     email = models.EmailField(max_length=100)
-    image = models.ImageField()
+     #image = models.ImageField()
--- fileupload/progress/views.py
+++ fileupload/progress/views.py
@@ -1,9 +1,13 @@
 from time import sleep
 from django.shortcuts import render
 from progress.forms import UserForm
+from progress.models import User
+
+
+
 # Create your views here.
 def index(request):
-    form = UserForm()
+    
 
-    context = {'form':form}
-    return render(request, 'index.html', context)
+    form = UserForm()
+    return render(request, 'index.html', {'form': form})
