13f6ebf597c9f9fcc50128342600285c5de5aa12
Allaye <kolade199@gmail.com>
2021-07-11 17:06:46 +0000

    views

--- fileupload/progress/views.py
+++ fileupload/progress/views.py
@@ -1,5 +1,7 @@
 from time import sleep
 from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required 
+from django.contrib.auth import authenticate, get_user_model, login, logout
 from progress.forms import UserForm, UserDetailsUpdate, UserLogin
 from progress.models import UserProfile
 
@@ -21,17 +23,24 @@ def index(request):
 def signin(request):
     return render(request, 'login.html')
 
+@login_required()
 def logout(request):
-    return redirect(index)
+    logout(request)
+    return redirect('index')
 
+
+@login_required()
 def profile(request):
     if request.method == 'POST':
-        form = UserDetailsUpdate(request.POST)
+        user = UserProfile.objects.get(user=request.user)
+        form = UserDetailsUpdate(request.POST, instance=user)
         if form.is_valid():
             form.save()
             return redirect('profile')
     form = UserDetailsUpdate()
     return render(request, 'profile.html', {'form': form})
+
+
 
 def registration(request):
     if request.method == "POST":
@@ -43,3 +52,22 @@ def registration(request):
             return redirect('login')
     userform = UserForm()
     return render(request, 'register.html', {'form': userform})
+
+
+
+
+# def myprofile(request):
+#     if request.method == 'POST':
+        
+#         form = UserProfileForm(request.POST, instance=request.user)
+#         if form.is_valid():
+#             form.save()
+#             user_profile = UserProfile.objects.get(user=request.user)
+#     if request.method == 'GET':
+#         user_profile = UserProfile.objects.get(user=request.user)
+#         form = UserProfileForm(instance=user_profile)
+
+# def myprofile(request):
+#     if request.method == 'POST':
+#         user_profile = UserProfile.objects.get(user=request.user)
+#         form = UserProfileForm(request.POST, instance=user_profile)
