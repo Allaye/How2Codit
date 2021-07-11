900e46d390fc11db23007e537b3f12df5608397e
Allaye <kolade199@gmail.com>
2021-07-11 17:48:14 +0000

    view

--- fileupload/progress/urls.py
+++ fileupload/progress/urls.py
@@ -4,7 +4,7 @@ from progress import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('signin/', views.signin, name='signin'),
-    path('logout/', views.logout, name='logout'),
+    path('logout/', views.signout, name='logout'),
     path('register/', views.registration, name='register'),
     path('profile/', views.profile, name='profile')
 
