11a4d77bf01c4326bb3be38184782298a3f7cb7f
Allaye <kolade199@gmail.com>
2021-07-11 10:49:37 +0000

    models

--- fileupload/progress/models.py
+++ fileupload/progress/models.py
@@ -1,7 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+
 
 # Create your models here.
-class User(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-    email = models.EmailField(max_length=100)
-     #image = models.ImageField()
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    sex = models.CharField(max_length=20)
+    website = models.URLField(blank=True)
+    image = models.ImageField(blank=True)
+
+    def __str__(self):
+        self.user.username
