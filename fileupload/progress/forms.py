f1d18a801c8a0498cf971cc9ff25ad82fd729dbb
Allaye <kolade199@gmail.com>
2021-07-11 10:49:09 +0000

    FORMS

--- fileupload/progress/forms.py
+++ fileupload/progress/forms.py
@@ -1,11 +1,24 @@
 from django import forms
-from django.forms import fields 
-from progress.models import User
+from django import forms
+from django.contrib.auth.models import User
+from django import forms
+from django.contrib.auth.models import User
+from django import forms
+from progress.models import UserProfile
 
 
 
 class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    
+    sex = forms.CharField()
+
     class Meta:
         model = User
-        fields = ['name', 'email',]
+        fields = ('username', 'email', 'password')
 
+
+class UserImageUpdate(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('image', 'website')
