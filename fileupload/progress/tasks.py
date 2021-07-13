4ef39b2ecdceae0a46df258486977383147ceee6
Allaye <kolade199@gmail.com>
2021-07-13 20:56:14 +0000

    tasks

--- fileupload/progress/tasks.py
+++ fileupload/progress/tasks.py
@@ -5,6 +5,12 @@ from celery_progress.backend import ProgressRecorder
 
 
 @shared_task(bind=True)
-def go_to_sleep(self, duration):
-    sleep(duration)
+def process_download(self):
+    progress_recoder = ProgressRecorder(self)
+    print('Start')
+    for i in range(5):
+        time.sleep(1)
+        print(i + 1)
+        progress_recoder.set_progress(i + 1, 5, description='downloading')
+    print('End')
     return 'done sleeping'
