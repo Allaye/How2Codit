1aeabf0ca8a1413c7fc4e5e672bf0f7297d35c8e
Allaye <kolade199@gmail.com>
2021-07-13 14:46:33 +0000

    app

--- fileupload/progress/apps.py
+++ fileupload/progress/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ProgressConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'progress'
+
+    def ready(self):
+        import progress.signals
