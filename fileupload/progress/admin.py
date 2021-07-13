d478c5ac532bfc6c46a6316e685d69275bfe7c96
Allaye <kolade199@gmail.com>
2021-07-13 14:45:52 +0000

    admin

--- fileupload/progress/admin.py
+++ fileupload/progress/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from progress.models import UserProfile
+from progress.models import Profile
 
 
 # Register your models here.
-admin.site.register(UserProfile)
+admin.site.register(Profile)
