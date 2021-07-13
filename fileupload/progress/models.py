48da73978ca6de308f557c9b48aad428b3def3db
Allaye <kolade199@gmail.com>
2021-07-13 14:47:22 +0000

    models

--- fileupload/progress/models.py
+++ fileupload/progress/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 
 
 # Create your models here.
-class UserProfile(models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     sex = models.CharField(max_length=20, blank=True)
     website = models.URLField(blank=True)
@@ -14,5 +16,12 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
-    def save(self):
-        super().save()
+    @receiver(post_save, sender=User)
+    def create_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+
+    @receiver(post_save, sender=User)
+    def save_profile(sender, instance, **kwargs):
+        instance.profile.save()
