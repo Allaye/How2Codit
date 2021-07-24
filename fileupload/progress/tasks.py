1b094e517a68d2e451440c9bc3f8f6982fb22f10
Allaye <kolade199@gmail.com>
2021-07-24 18:29:21 +0000

    task

--- fileupload/progress/tasks.py
+++ fileupload/progress/tasks.py
@@ -1,6 +1,8 @@
 import io
+import math
 import sys
 import time
+from PIL import Image
 from celery import shared_task
 from celery_progress.backend import ProgressRecorder
 from django.core.files.storage import FileSystemStorage
@@ -18,12 +20,6 @@ from django.core.files.images import ImageFile
 #         progress_recoder.set_progress(i + 1, 5, description='downloading')
 #     print('End')
 #     return 'done sleeping'
-def read_chunk(file_object, chunk_size=125):
-    while True:
-        file =  file_object.read(chunk_size)
-        if not file:
-            break
-        yield file
 
 @shared_task(bind=True)
 def process_download(self, image_file):
@@ -35,13 +31,50 @@ def process_download(self, image_file):
     for chunk in image_file.chunks():  
         chunk_size += len(chunk)      
         buffer.write(chunk)
-        # length = len(list(image_file.chunks()))
         process_recoder.set_progress(chunk_size, image_file.size, description=f'uploaded {chunk_size} bytes of the file')
     buffer.seek(0)
     image = ImageFile(buffer, name=image_file.name)
     fs.save(image_file.name, content=image)
     return 'Done'
 
+@shared_task(bind=True)
+def process_download_with_progress(self, image_file, length):
+    process_recoder = ProgressRecorder(self)
+    print('Upload: Task Started')
+    fs = FileSystemStorage()
+    buffer = io.BytesIO()
+    chunk_size = 0
+    for chunk in read_chunk(image_file.file, length):  
+        chunk_size += 1      
+        buffer.write(chunk)
+        if chunk_size == 1:
+            length = math.ceil((len(image_file.file.getvalue()))/10000)
+        process_recoder.set_progress(chunk_size, length, description=f'uploaded {chunk_size*length} bytes of the file')
+    buffer.seek(0)
+    image = io.BytesIO(buffer.getvalue())
+    image_file = ImageFile(image, name=image_file.name)
+    fs.save(image_file.name, image)
+    return 'Done'
+# def simple_upload(request):
+#     if request.method == 'POST' and request.FILES['image']:
+#         myfile = request.FILES['image']
+#         fs = FileSystemStorage()
+#         buffer = io.BytesIO()
+#         for chunk in read_chunk(myfile.file, 125):
+#             buffer.write(chunk)
+#         buffer.seek(0)
+#         buffer = io.BytesIO(buffer.getvalue())
+#         #buffer = buffer.getvalue()
+#         image = ImageFile(buffer, 'name.jpg')
+#         fs.save(myfile.name, image)
+#         return render(request, 'index.html')
+#     return render(request, 'upload.html')
+def read_chunk(file_object, chunk_size=125):
+    while True:
+        file =  file_object.read(chunk_size)
+        if not file:
+            break
+        yield file
 
 
 
