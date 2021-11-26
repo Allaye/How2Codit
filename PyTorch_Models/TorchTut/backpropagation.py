54932e40d8759ca9cfbf6d32a144ba275c735b36
Allaye <kolade199@gmail.com>
2021-11-26 21:01:13 +0100

    changes to backpropagation

--- PyTorch_Models/TorchTut/backpropagation.py
+++ PyTorch_Models/TorchTut/backpropagation.py
@@ -15,3 +15,10 @@ def backpropagation(x, y, w):
     # update the weights
 
 
+if __name__ == "__main__":
+    # create respective tensor with the weight have grad func activated
+    x = torch.tensor(1.0)
+    y = torch.tensor(2.0)
+    w = torch.tensor(1.0, requires_grad=True)
+
+    backpropagation(x, y, w)
