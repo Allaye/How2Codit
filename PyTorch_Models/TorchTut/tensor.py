ccf9c011ca0d87ef242d53708fd989defe91689e
Allaye <kolade199@gmail.com>
2021-11-26 19:37:36 +0100

    basic torch usage

--- PyTorch_Models/TorchTut/tensor.py
+++ PyTorch_Models/TorchTut/tensor.py
@@ -18,5 +18,22 @@ t_np = torch.from_numpy(np_data)
 # create a new tensor from tensor data
 one_t = torch.ones_like(t_np)
 
-# create a new random tensor from tensor data
-rand_t = torch.rand_like(t_np)
+# create a new random tensor from tensor data, and change the data type to float
+rand_t = torch.rand_like(t_np, dtype=torch.float32)
+
+#indexing and slicing
+tensor = torch.ones(3,3)
+print("first row", tensor[0])
+print("first column", tensor[:,0])
+
+# reshape tensor
+x = tensor.view(9)
+y = tensor.view(-1)
+
+
+# arithmatic operations on tensor
+
+y1 = tensor @ tensor
+y2 = tensor.matmul(tensor)
+y3 = torch.rand_like(tensor)
+torch.matmul(tensor, tensor.T, out=y3)
