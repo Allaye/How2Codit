9be02384d1e556e64cfe176d7d99a4e3b5881e00
Allaye <kolade199@gmail.com>
2021-11-27 17:23:52 +0100

    update to torch gredient

--- PyTorch_Models/TorchTut/torch_gradient.py
+++ PyTorch_Models/TorchTut/torch_gradient.py
@@ -1,43 +1,68 @@
 import torch
+import torch.nn as nn
 
-X = torch.tensor([1,2,3,4], dtype=torch.float32)
-Y = torch.tensor([2,4,6,8], dtype=torch.float32)
-w = torch.tensor(0.0, dtype=torch.float32, requires_grad=True)
 
-def forward(X, w=torch.tensor(0.0 , dtype=torch.float32, requires_grad=True)):
-    # calculate the forward function to calculate for the predicted value
-    return X * w
+# pytorch nn pipeline
+# 1. define the model, design the input, output layer
+# 2. construct the loss function and the optimizer
+# 3. train the model
+     # 3.1 forward pass (forward propagation): compute the modelprediction
+     # 3.2 backward pass (backward propagation): compute the gradient of the loss function
+     # 3.3 update the parameters (update the model parameters like weight and bias)
+
+
+
+X = torch.tensor([[1],[2],[3],[4]], dtype=torch.float32)
+Y = torch.tensor([[2],[4],[6],[8]], dtype=torch.float32)
+
+test = torch.tensor([5], dtype=torch.float32)
+
+
+def forward(X):
+    _, n_features = X.shape
+    input_size = n_features
+    output_size = n_features
+    return nn.Linear(input_size, output_size)
 
 def loss(Y, Y_predicted):
-    # loss MSE 
-    # MSE = 1/N * (w*x -y)**2
-    # where w*x is the value of the forward function
-    return ((Y_predicted - Y) ** 2).mean()
+    # calculate loss MSE
+    l = nn.MSELoss()
+    return l(Y, Y_predicted)
 
 def gradient(l):
-    # calculate = backwardpass 
+    # calculate = backwardpass
     # pytorch calculates the local derivative of the loss function
     return l.backward()
 
-def training(lr, iters, X, Y, w):
-    print(f"The predicted Y before training for value X: 5 is {forward(5, w):.2f}")
+def optimize(w, lr=0.01):
+    # define the optimizer
+    # update the weight by moving in the negative direction of the gradient
+
+    return torch.optim.SGD(w, lr=0.01)
+
+
+def training(lr, iters, X, Y):
+    print(f"The predicted Y before training for value X: 5 is {forward(test.item()):.2f}")
     for epoch in range(iters):
         # forward pass, calculate the predicted value
-        Y_pred = forward(X, w)
+        Y_pred = forward(X)
         # calculate the loss
         los = loss(Y, Y_pred)
         # calculate the gradient
         gradient(los)
         # update the weight by moving in the negetive direction of the gradient
-        with torch.no_grad():
-            w -= lr * w.grad
-        w.grad.zero_()
+        optimizer = optimize(Y_pred.parameters(), lr)
+        optimizer.step()
+        # with torch.no_grad():
+        #     w -= lr * w.grad
+        optimizer.zero_grad()
         if epoch % 10 == 0:
-            print("epoch: {}, loss: {:.8f}, w: {}".format(epoch, los, w))
-    print(f"The predicted Y After training for value X: 5 is {forward(5, w):.2f}")
+            w, b = Y_pred.parameters()
+            print("epoch: {}, loss: {:.8f}, w: {}".format(epoch, los, w[0][0].item()))
+    print(f"The predicted Y After training for value X: 5 is {forward(test.item()):.2f}")
 
 
 if __name__ == "__main__":
-    training(lr=0.01, iters=100, X=X, Y=Y, w=w)
+    training(lr=0.01, iters=100, X=X, Y=Y)
 
 
