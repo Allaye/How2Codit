42d6f4c2b7aaa5e1849d30b24bbea0e8f6019459
Allaye <kolade199@gmail.com>
2021-12-01 14:25:00 +0100

    update on dataloader

--- PyTorch_Models/TorchTut/datasets_and_dataloader.py
+++ PyTorch_Models/TorchTut/datasets_and_dataloader.py
@@ -15,9 +15,11 @@ from torch.utils.data import Dataset, DataLoader
 
 
 
-class CustomDataset(Dataset):
+class CustomDatasetLoader(Dataset):
     """
     A class to create a custom dataset, which is a subclass of torch.utils.data.Dataset
+    and a data loader, which is a subclass of torch.utils.data.DataLoader
+
     
     """
     def __init__(self, data_path, delimiter=',', dtype=np.float32, skiprows=0):
@@ -39,7 +41,20 @@ class CustomDataset(Dataset):
         """
         return self.n_samples
 
+    def loader(self, batch_size=5, shuffle=True, num_workers=0):
+        """
+        create a customizable data loader
+        """
+        return DataLoader(dataset=self, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
 
 
 
+data = CustomDatasetLoader(data_path='./wine.csv', skiprows=1)
+first_data = data[0]
+features, labels = first_data
+print(features, labels, ';;;;;')
+
+dataloader = data.loader()
+next_batch = next(iter(dataloader))
+print(next_batch)
 
